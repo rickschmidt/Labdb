b33ebbe329b8bc4acb544459f9b69f19c4935248
Rick Schmidt <rickschmidt@mbp.gateway.2wire.net>
2011-03-08 17:53:46 -0600

    changed tax model

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -2,6 +2,6 @@ class Taxonomy < ActiveRecord::Base
   has_and_belongs_to_many :dnasamples
   
     def self.search(search)
-          @search=Taxonomy.id_like("")  #Search is not built yet
+          @search=Taxonomy.genus_like("")  #Search is not built yet
       end
 end
