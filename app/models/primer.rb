d9f52e49dcd05cb7e60d432855de7abc995b9e12
Rick Schmidt <rickschmidt@mbp.gateway.2wire.net>
2011-04-20 16:50:09 -0500

    cssed primer table

--- app/models/primer.rb
+++ app/models/primer.rb
@@ -1,3 +1,11 @@
 class Primer < ActiveRecord::Base
     has_and_belongs_to_many :genes
+    
+        def self.search(search)
+           if search
+              @search=Primer.primerl_or_primerh_or_id_like_any(search)  #Search is not built yet
+              else
+                @search=Primer.primerl_like("")
+          end
+      end
 end
