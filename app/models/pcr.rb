b5c93ac7548c47ec3d57506a86d0b9e0e99f5d90
Rick Schmidt <rickschmidt@MBP.local>
2011-05-09 14:09:06 -0500

    Edited layouts in multiple files

--- app/models/pcr.rb
+++ app/models/pcr.rb
@@ -2,5 +2,16 @@ class Pcr < ActiveRecord::Base
   has_and_belongs_to_many :experiments
   has_and_belongs_to_many :dnasamples
   
+   def self.search(search)
+        if search
+          
+          Pcr.primerh_like_any(search)
+        else
+          @search=Pcr.id_like("")
+
+          
+        end
+      end
+  
   
 end
