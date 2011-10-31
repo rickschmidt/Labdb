814ccebc49a87f882bd82e68189b200a658d1b93
Rick Schmidt <rick@rickschmidt.org>
2011-10-31 16:29:35 -0500

    Created Taxa association in DNASAMPLE NEW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	protected
+	
+	def layout_by_resource
+    	if devise_controller?
+
+	    else
+	      "application"
+	    end
+  	end
 end
